fb7e40f4b4eaf83425e4b2926e38ead86706160c
Abhiraj Gaikwad <154492869+Abhi-0939@users.noreply.github.com>
2024-02-17 07:45:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -116,17 +116,17 @@ var swiper = new Swiper(".client-swiper", {
   ScrollReveal({ 
     // reset: true ,
     distance : '60px',
-    duration : 2500,
-    delay: 100
+    duration : 2000,
+    delay: 70
   });
 
-  ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay : 500, origin: 'left' });
-  ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay : 600, origin: 'right' });
+  ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay : 400, origin: 'left' });
+  ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay : 500, origin: 'right' });
   ScrollReveal().reveal('.home .info .btn', { delay : 700, origin: 'bottom'});
-  ScrollReveal().reveal('.media-icons i, .contact-left li', { delay : 500, origin: 'left', interval: 200 });
+  ScrollReveal().reveal('.media-icons i, .contact-left li', { delay : 400, origin: 'left', interval: 150 });
   ScrollReveal().reveal('.home-img,about-img ', { delay : 500, origin: 'bottom' });
-  ScrollReveal().reveal('.about .description, .contact-right ', { delay : 600, origin: 'right' });
-  ScrollReveal().reveal('.about .professional-list li ', { delay : 500, origin: 'right' , interval:200});
-  ScrollReveal().reveal('.skills-description, .service-description, .contact-card, .contact-left h2 ', { delay : 700, origin: 'left'});
-  ScrollReveal().reveal('.experience-card, .service-card, .education, .img-card', { delay : 800, origin: 'bottom', interval:200});
-  ScrollReveal().reveal('footer .group', { delay : 500, origin: 'top', interval:200});
+  ScrollReveal().reveal('.about .description, .contact-right ', { delay : 500, origin: 'right' });
+  ScrollReveal().reveal('.about .professional-list li ', { delay : 400, origin: 'right' , interval:150});
+  ScrollReveal().reveal('.skills-description, .service-description, .contact-card, .contact-left h2 ', { delay : 600, origin: 'left'});
+  ScrollReveal().reveal('.experience-card, .service-card, .education, .img-card', { delay : 700, origin: 'bottom', interval:150});
+  ScrollReveal().reveal('footer .group', { delay : 400, origin: 'top', interval:150});
